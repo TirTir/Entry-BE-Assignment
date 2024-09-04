de3e9f53044a99273e55375851a34149a234bdc4
yujin <uj0791navercom@skuniv.ac.kr>
2024-09-04 17:19:21 +0900

    project base setup

---
+++ src/test/java/backend/keumbang/KeumbangApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.keumbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeumbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
