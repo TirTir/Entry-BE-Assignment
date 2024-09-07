1c578a57fd78a8e67f591b1230f7ebfa4f025e8c
yujin <uj0791navercom@skuniv.ac.kr>
2024-09-07 10:46:29 +0900

    [FEAT] 유효성 검사

---
+++ src/main/java/backend/keumbang/auth/dto/RegisterRequestDto.java
@@ -0,0 +1,24 @@
+package backend.keumbang.auth.dto;
+
+import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RegisterRequestDto {
+    @NotBlank(message = "이름을 입력해주세요.")
+    @Size(min = 2, max = 8, message = "이름을 2~8자 사이로 입력해주세요.")
+    private String userName;
+
+    // 비밀번호(영문, 특수문자, 숫자 포함 8자 이상)
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*[$@$!%*#?&.])[A-Za-z$@$!%*#?&.]{8,20}$",
+            message = "비밀번호는 영문자와 특수문자를 포함하여 8자 이상 20자 이하로 입력해주세요."
+    )
+    private String password;
+}
