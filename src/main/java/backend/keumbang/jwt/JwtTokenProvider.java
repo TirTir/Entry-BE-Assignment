1fdd442a78df3ec0ab07521ec4ceaf4fb44285b6
yujin <uj0791navercom@skuniv.ac.kr>
2024-09-09 01:15:52 +0900

    [FEAT] Jwt 유효성 검사

--- src/main/java/backend/keumbang/jwt/JwtTokenProvider.java
+++ src/main/java/backend/keumbang/jwt/JwtTokenProvider.java
@@ -1,9 +1,7 @@
 package backend.keumbang.jwt;
 
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @Component
 public class JwtTokenProvider {
@@ -33,6 +33,10 @@ public class JwtTokenProvider {
 
         this.accessExpirationTime = accessExpirationTime;
         this.refreshExpirationTime = refreshExpirationTime;
+    }
+
+    public SecretKey getSecretKey() {
+        return this.secretKey;  // 시크릿 키 반환
     }
 
     /**
@@ -69,4 +73,28 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    /**
+     * 토큰 유효성 검사
+     */
+    public boolean validateToken(String jwtToken){
+        try {
+            Jwts.parser()
+                    .setSigningKey(secretKey.getEncoded())  // 서명 검증에 사용할 시크릿 키 설정
+                    .build()
+                    .parseClaimsJws(jwtToken)
+                    .getBody();  // 서명 검증
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
