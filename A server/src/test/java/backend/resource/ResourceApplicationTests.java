e22fa7fe98421b84a8d8eec0901a0330d82d537c
yujin <uj0791navercom@skuniv.ac.kr>
2024-09-10 10:35:08 +0900

    [FEAT] A server

---
+++ A server/src/test/java/backend/resource/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ B server/src/main/java/backend/keumbang/KeumbangApplication.java
@@ -0,0 +1,13 @@
+package backend.keumbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeumbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeumbangApplication.class, args);
+	}
+
+}
